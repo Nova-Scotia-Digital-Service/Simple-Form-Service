878a257aa8dc0615f1e21d02f18b64c986acd827
pmccabensds <patrick.mccabe@novascotia.ca>
2021-11-07 19:13:29 -0400

    Fixing gitignore

---
+++ SimpleFormsService.Web.Public/wwwroot/js/site.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+    $('.input-validation-error').parents('.form-group').addClass('has-error');
+    $('.input-validation-error').parents('.form-group').addClass('ns-fieldset-message-error');
+    $('.field-validation-error').addClass('errMsg');
+});
