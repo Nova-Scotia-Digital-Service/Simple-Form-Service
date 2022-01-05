043139e390f306649889e19d726cf9bdd3ad5c88
Badcock, Paul <paul.badcock@novascotia.ca>
2022-01-05 15:43:54 -0400

    this fixes the environment variable usage

--- SimpleFormsService.Configuration/AppConfig.cs
+++ SimpleFormsService.Configuration/AppConfig.cs
@@ -7,7 +7,7 @@ public static class AppConfig
 {
     public static IConfigurationRoot SharedSettings => LazyConfig.Value;
 
-    public static string Postgres_ConnectionString => string.IsNullOrEmpty(Environment.GetEnvironmentVariable("connection-string")) ? SharedSettings.GetSection("PostgreSQL:ConnectionString").Value : Environment.GetEnvironmentVariable("connection-string");
+    public static string Postgres_ConnectionString => Environment.GetEnvironmentVariable("POSTGRES_CONNECT_STRING");
 
     private static readonly Lazy<IConfigurationRoot> LazyConfig = new(() => new ConfigurationBuilder()
         .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
