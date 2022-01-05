327b29449bb18e9bcb9ec8dd15fc7e9f94e9621b
Badcock, Paul <paul.badcock@novascotia.ca>
2022-01-05 16:04:18 -0400

    getting to the openshift logic on looking for POSTGRES_CONNECT_STRING was missing this allows for connect-string to work also as expected

--- SimpleFormsService.Configuration/AppConfig.cs
+++ SimpleFormsService.Configuration/AppConfig.cs
@@ -7,7 +7,7 @@ public static class AppConfig
 {
     public static IConfigurationRoot SharedSettings => LazyConfig.Value;
 
-    public static string Postgres_ConnectionString => Environment.GetEnvironmentVariable("POSTGRES_CONNECT_STRING");
+    public static string Postgres_ConnectionString => OpenshiftConfig.Postgres_ConnectionString;
 
     private static readonly Lazy<IConfigurationRoot> LazyConfig = new(() => new ConfigurationBuilder()
         .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
