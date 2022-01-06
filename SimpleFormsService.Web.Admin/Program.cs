09bdfda6cc7b0022a8195269dd76eec9014e8b99
yangih <inica.yang@novascotia.ca>
2022-01-06 09:50:09 -0400

    Azure role claim

--- SimpleFormsService.Web.Admin/Program.cs
+++ SimpleFormsService.Web.Admin/Program.cs
@@ -15,9 +15,7 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
             options.ClientId = string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("CLIENT_ID")) ? builder.Configuration["AzureAd:ClientId"] : Environment.GetEnvironmentVariable("CLIENT_ID");
             options.TenantId = string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("TENANT_ID")) ? builder.Configuration["AzureAd:TenantId"] : Environment.GetEnvironmentVariable("TENANT_ID");
             options.Instance = string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("AZURE_AD_INSTANCE")) ? builder.Configuration["AzureAd:Instance"] : Environment.GetEnvironmentVariable("AZURE_AD_INSTANCE");
-            options.CallbackPath = string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("CALL_BACK_PATHd")) ? builder.Configuration["AzureAd:CallbackPath"] : Environment.GetEnvironmentVariable("CALL_BACK_PATH");
-            //for roles
-            options.TokenValidationParameters.RoleClaimType = "groups";
+            options.CallbackPath = string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("CALL_BACK_PATH")) ? builder.Configuration["AzureAd:CallbackPath"] : Environment.GetEnvironmentVariable("CALL_BACK_PATH");
         },
         cookieOptions =>
         {
@@ -37,12 +35,8 @@ builder.Services.AddAuthorization(policies =>
 {
     policies.AddPolicy("GroupAdmin", p =>
     {
-        p.RequireClaim("groups", "SFS-Admin");
+        p.RequireClaim("groups", Environment.GetEnvironmentVariable("GROUP_ADMIN_ID"));
     });
-    //policies.AddPolicy("UserPolicy", p =>
-    //{ 
-    //    p.RequireClaim("roles", "SFS-User"); 
-    //});
 });
 
 // Add services to the container.
