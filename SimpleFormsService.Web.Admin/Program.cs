1283ce53cf51e8b58a9b51185f8e0e943e0fedbd
Yang, Inica H Y <inica.yang@novascotia.ca>
2022-01-10 21:07:32 -0400

    Admin UI sign out function

--- SimpleFormsService.Web.Admin/Program.cs
+++ SimpleFormsService.Web.Admin/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
             options.ClientId = string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("CLIENT_ID")) ? builder.Configuration["AzureAd:ClientId"] : Environment.GetEnvironmentVariable("CLIENT_ID");
             options.TenantId = string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("TENANT_ID")) ? builder.Configuration["AzureAd:TenantId"] : Environment.GetEnvironmentVariable("TENANT_ID");
             options.Instance = string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("AZURE_AD_INSTANCE")) ? builder.Configuration["AzureAd:Instance"] : Environment.GetEnvironmentVariable("AZURE_AD_INSTANCE");
-            options.CallbackPath = string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("CALL_BACK_PATH")) ? builder.Configuration["AzureAd:CallbackPath"] : Environment.GetEnvironmentVariable("CALL_BACK_PATH");
+            options.CallbackPath = string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("CALL_BACK_PATH")) ? builder.Configuration["AzureAd:CallbackPath"] : Environment.GetEnvironmentVariable("CALL_BACK_PATH");           
         },
         cookieOptions =>
         {
@@ -59,7 +59,7 @@ builder.Services.AddAuthorization(policies =>
 {
     policies.AddPolicy("GroupAdmin", p =>
     {
-    p.RequireClaim("groups", Environment.GetEnvironmentVariable("GROUP_ADMIN_ID"));
+        p.RequireClaim("groups", Environment.GetEnvironmentVariable("GROUP_ADMIN_ID"));
     });
 });
 
@@ -109,6 +109,12 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapRazorPages();
+});
 
 app.Run();
